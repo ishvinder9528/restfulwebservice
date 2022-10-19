41f653e23e662da3a16b680791c78c3c3a77aebb
ishvinder9528 <ishvinder133@gmail.com>
2022-10-19 13:20:04 +0530

    initaial commit

--- src/main/java/com/ishu/rest/webservice/restfulwebservice/todo/TodoHardcodedService.java
+++ src/main/java/com/ishu/rest/webservice/restfulwebservice/todo/TodoHardcodedService.java
@@ -13,7 +13,7 @@ public class TodoHardcodedService {
     private static int idCounter = 0;
 
     static {
-        todos.add(new Todo(++idCounter, "ishvinder", "Learn Java", new Date(), false));
+        todos.add(new Todo(++idCounter, "ishvinder", "Learn Java", new Date(), true));
         todos.add(new Todo(++idCounter, "ishvinder", "Learn Angualar", new Date(), false));
         todos.add(new Todo(++idCounter, "ishvinder", "Learn Spring", new Date(), false));
     }
@@ -21,4 +21,25 @@ public class TodoHardcodedService {
     public List<Todo> findAll() {
         return todos;
     }
+
+    public Todo deleteById(long id) {
+        Todo todo = findById(id);
+        if (todo == null) {
+            return null;
+        }
+        if (todos.remove(todo)) {
+            return todo;
+        }
+        return null;
+    }
+
+    public Todo findById(long id) {
+        for (Todo todo : todos) {
+            if (todo.getId() == id) {
+                return todo;
+            }
+        }
+        return null;
+    }
+
 }
--- src/main/java/com/ishu/rest/webservice/restfulwebservice/todo/TodoResource.java
+++ src/main/java/com/ishu/rest/webservice/restfulwebservice/todo/TodoResource.java
@@ -3,10 +3,15 @@ package com.ishu.rest.webservice.restfulwebservice.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TodoResource {
 
@@ -16,5 +21,15 @@ public class TodoResource {
     @GetMapping(path = "/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username) {
         return todoService.findAll();
+    }
+
+    @DeleteMapping(path = "/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
+        Todo todo = todoService.deleteById(id);
+        if (todo != null) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+
     }
 }
